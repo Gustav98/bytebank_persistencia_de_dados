bb092076df63028f1c553d23555fd52327f9c1bb
T806572 <vermont.gustavo@tokiomarine.com.br>
2022-04-18 17:02:09 -0300

    Implementando lista de transações

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -3,6 +3,8 @@
 import 'package:bytebank_flutter2/screens/contacts_list.dart';
 import 'package:flutter/material.dart';
 
+import 'transactions_list.dart';
+
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -18,45 +20,86 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('images/bytebank_logo.png'),
           ),
-          Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Material(
-                color: Theme.of(context).primaryColor,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      (MaterialPageRoute(
-                        builder: (context) => ContactsList(),
-                      )),
-                    );
+          SizedBox(
+            height: 120,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                _FeatureItem(
+                  'Transfer',
+                  Icons.monetization_on,
+                  onClick: () {
+                    _showContactsList(context);
                   },
-                  child: Container(
-                    padding: EdgeInsets.all(8.0),
-                    height: 100,
-                    width: 150,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Icon(
-                          Icons.people,
-                          color: Colors.white,
-                          size: 24.0,
-                        ),
-                        Text(
-                          'Contacts',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
                 ),
-              ))
+                _FeatureItem(
+                  'Transaction feed',
+                  Icons.description,
+                  onClick: () => _showTransactionList(context),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
+
+  _showTransactionList(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => TransactionsList(),
+      ),
+    );
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  final Function onClick;
+
+  const _FeatureItem(this.name, this.icon, {required this.onClick});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () => onClick(),
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  name,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void _showContactsList(BuildContext context) {
+  Navigator.of(context).push(
+    (MaterialPageRoute(
+      builder: (context) => ContactsList(),
+    )),
+  );
 }
